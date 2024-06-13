e237d69c720eec131b3eb03ec0d8d4aaf55bb154
ErzeKawek <130212655+ErzeKawek@users.noreply.github.com>
2024-06-13 19:01:37 +0700

    Still Raw

---
+++ src/main/java/net/erzekawek/saturatedextras/SaturatedExtras.java
@@ -0,0 +1,16 @@
+package net.erzekawek.saturatedextras;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SaturatedExtras implements ModInitializer {
+	public static final String MOD_ID = "saturated_extras";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
